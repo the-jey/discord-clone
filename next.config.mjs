4772e8bb1efc90e781f42a01b3e645ef151c83b2
JEY <jey.pley@gmail.com>
2024-01-26 14:25:10 +0100

    uploadthing setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["uploadting.com", "utfs.io"],
+  },
+};
 
 export default nextConfig;
